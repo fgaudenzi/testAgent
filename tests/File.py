f3064ad1c0ac0904130d15d7aefcf7aab0d06c24
Patrizio Tufarolo <patrizio@tufarolo.eu>
2014-10-14 10:23:43 +0200

    updated file.py

--- tests/File.py
+++ tests/File.py
@@ -57,59 +57,102 @@ if __name__ == '__main__':
 
 	tester = Tester(host,username_owner,ssh_key_path,ssh_server_key_path,filepath,chmod,port) #REVIEW del prototipo
 	
-	print "[CHECKING CHMOD]"
+
+	step11_status = False
+	step12_status = False
+	step13_status = False
+	step1_status = False
+
+	#print "[CHECKING CHMOD]"
 	if tester.checkChmod() == True:
-		print "success"
+		step11_status = True
+		#print "success"
 	else:
-		print "failed"
+		print "False"
 		sys.exit(1)
 
-	print "[TESTING OWNER READ]"
+	#print "[TESTING OWNER READ]"
 	if tester.checkRead((chmod_owner)[0]) == True:
-		print "success"
+		step12_status = True
+		#print "success"
 	else:
-		print "failed"
-	print "[TESTING OWNER WRITE]"
+		pass
+		#print "failed"
+	#print "[TESTING OWNER WRITE]"
 	if tester.checkWrite((chmod_owner)[1]) == True:
-		print "success"
+		step13_status = True
+		#print "success"
 	else:
-		print "failed"
+		pass
+		#print "failed"
+
+
+	if step11_status and step12_status and step13_status:
+		step1_status = True
+
 
 
 	username_group	=	parser.get("3","username_group")
 	tester = Tester(host,username_group,ssh_key_path,ssh_server_key_path,filepath,chmod,port) #REVIEW del prototipo
-	
-	print "[TESTING GROUP READ]"
+
+
+	step21_status = False
+	step22_status = False
+	step2_status = False
+
+	#print "[TESTING GROUP READ]"
 	sleep(1)
 	if tester.checkRead((chmod_group)[0]) == True:
-		print "success"
+	#	print "success"
+		step21_status = True
 	else:
-		print "failed"
+		pass
+    #	print "failed"
 
 	print "[TESTING GROUP WRITE]"
 	sleep(1)
 	if tester.checkWrite((chmod_group)[1]) == True:
-		print "TESTING GROUP: write check success"
+		#print "TESTING GROUP: write check success"
+		step22_status = True
 	else:
-		print "TESTING GROUP: write check failed"
+		pass
+		#print "TESTING GROUP: write check failed"
+
+	if step21_status and step22_status:
+		step2_status = True
 
 	username_other	=	parser.get("4","username_other")
 	tester = Tester(host,username_other,ssh_key_path,ssh_server_key_path,filepath,chmod,port) #REVIEW del prototipo
 	
-	print "[TESTING OTHER READ]"
+	#print "[TESTING OTHER READ]"
+
+	step31_status = False
+	step32_status = False
+	step3_status = False
+
 	sleep(1)
 	if tester.checkRead((chmod_other)[0]) == True:
-		print "TESTING OTHER: read check success"
+		step31_status  = True
+	#	print "TESTING OTHER: read check success"
 	else:
-		print "TESTING OTHER: read check failed"
+		pass
+	#	print "TESTING OTHER: read check failed"
 
-	print "[TESTING OTHER WRITE]"
+	#print "[TESTING OTHER WRITE]"
 	sleep(1)
 	if tester.checkWrite((chmod_other)[1]) == True:
-		print "TESTING OTHER: write check success"
+		step32_status = True
+	#	print "TESTING OTHER: write check success"
 	else:
-		print "TESTING OTHER: write check failed"
+		pass
+	#	print "TESTING OTHER: write check failed"
+	if step31_status and step32_status:
+		step3_status = True
 
+	if step1_status and step2_status and step3_status:
+		print "True"
+	else:
+		print "False"
 
 
 
